42a5abed33388e01696850ce8ab35ed3628c29a4
Akpe Caleb <akpec116@gmail.com>
2022-10-19 11:13:21 -0700

    ne

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - prints the alphabet, in lowercase
+ * followed by a new line
+ * Always: Return 0
+ */
+void print_alphabet_x10(void);
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+		_putchar(la);
+
+	_putchar('\n');
+}
